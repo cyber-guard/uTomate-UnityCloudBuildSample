7525a3de7f22f6c2af74b481058709645095c989
Simon Vlcek <contact@cyber-guard.co.uk>
2017-09-06 09:27:32 +0200

    something

--- Assets/CloudBuildIntegration/Editor/Integration/UTCloudBuildRunner.cs
+++ Assets/CloudBuildIntegration/Editor/Integration/UTCloudBuildRunner.cs
@@ -3,56 +3,17 @@ namespace AncientLightStudios.UTomate.CloudBuild
     using System;
     using System.Collections;
     using UnityEngine;
-    using UnityEngine.Networking;
  
     public class UTCloudBuildRunner
     {
         public static void OnPreExport()
         {
-            Debug.Log("Testing 1234");
+            Debug.LogWarning("Testing 1234");
         }
 
         public static void OnPostExport(string currentlyIgnored)
         {
-            var plan = UTUnityCloudBuildConfiguration.UnityCloudBuildConfiguration.runOnPostExport;
-            if (plan != null)
-            {
-                PlanRunHack(plan);
-            }
-            else
-            {
-                Debug.Log("No plan for post-export. Skipping.");
-            }
-        }
-
-        private static void PlanRunHack(UTAutomationPlan plan)
-        {
-            // this is a hack around Unity Cloud Build not supporting coroutine semantics.
-            // will be removed once coroutine semantics are there.
-            var enumerator = RunPlan(plan);
-            while (enumerator.MoveNext())
-            {
-                // loop until done.
-            }
-        }
-
-        private static IEnumerator RunPlan(UTAutomationPlan plan)
-        {
-            UTPreferences.DebugMode = UTUnityCloudBuildConfiguration.UnityCloudBuildConfiguration.debugMode;
-
-            Debug.LogWarning("About to run the plan: " + plan.name);
-            var start = Time.realtimeSinceStartup;
-
-            global::UTomate.Run(plan);
-
-            var took = Time.realtimeSinceStartup - start;
-            while (UTomateRunner.Instance.IsRunning)
-            {
-                UTomateRunner.Instance.ContinueRunning();
-                yield return null;
-            }
-
-            Debug.LogWarning("Plan ran. Took " + took + "ms");
+            Debug.LogWarning("No plan for post-export. Skipping.");
         }
     }
 }
